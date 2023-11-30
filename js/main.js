25d2bee56f5d4737ae4a2b13dea47aefd63ccecb
yeonjeuk <duswpdnr9@gmail.com>
2023-11-30 09:53:11 +0900

    탭메뉴 수정

--- js/main.js
+++ js/main.js
@@ -40,6 +40,34 @@ Array.from(tabButtons3).forEach((eachButton, index) => {
 });
 tabButtons3[0].click();
 
+let tabButtons4 = document.querySelectorAll('.tabButton4');
+Array.from(tabButtons4).forEach((eachButton, index) => {
+  eachButton.addEventListener('click', function () {
+    let tabBoxs4 = document.querySelectorAll('.tabBox4');
+    for (var i = 0; i < tabBoxs4.length; i++) {
+      tabBoxs4[i].classList.remove("on");
+      tabButtons4[i].classList.remove("on");
+    }
+    tabBoxs4[index].classList.add("on");
+    this.classList.add("on");
+  });
+});
+tabButtons4[0].click();
+
+let tabButtons5 = document.querySelectorAll('.tabButton5');
+Array.from(tabButtons5).forEach((eachButton, index) => {
+  eachButton.addEventListener('click', function () {
+    let tabBoxs5 = document.querySelectorAll('.tabBox5');
+    for (var i = 0; i < tabBoxs5.length; i++) {
+      tabBoxs5[i].classList.remove("on");
+      tabButtons5[i].classList.remove("on");
+    }
+    tabBoxs5[index].classList.add("on");
+    this.classList.add("on");
+  });
+});
+tabButtons5[0].click();
+
 const Profile = document.querySelector('.Profile-click');
 Profile.onclick = function () {
   window.open(this.href, '_blank', 'width=800, height=600'); return false;;
